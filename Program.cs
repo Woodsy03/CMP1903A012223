8c2e650e74d0793fe258e395a9ccd1068fb4c07c
Woodsy03 <119528792+Woodsy03@users.noreply.github.com>
2023-03-03 14:41:30 +0000

    ordered Deck Generation

--- Program.cs
+++ Program.cs
@@ -10,7 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<string> Numbers = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
+            List<string> Suites = new List<string>() { "Heart", "Spade", "Club", "Diamond" };
 
+            // Ordered Deck Creation
+            List<string> Deck = new List<string>();
+            foreach (string x in Suites)
+                foreach (string y in Numbers)
+                    Deck.Add(y + " of " + x);
+
+            // 
+
+
+
+
+
+            // Prints Shuffled Output
+            for (int i = 0; i < Deck.Count; i++)
+            {
+                Console.WriteLine(Deck[i]);
+            }
 
         }
     }
